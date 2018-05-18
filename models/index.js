2ec7ccd1fcb792c0dee035a677c16437a56a8a9f
victordepablo <victor.depablo.97@gmail.com>
2018-05-18 13:26:59 +0200

    P6_Quiz index models preguntas base de datos

--- models/index.js
+++ models/index.js
@@ -19,12 +19,33 @@ sequelize.import(path.join(__dirname, 'quiz'));
 // Session
 sequelize.import(path.join(__dirname,'session'));
 
+//Creacion quizzes
+
+sequelize.sync()
+    .then(()=>
+sequelize.models.quiz.count()
+)
+.then(count=>{
+    if(!count){
+    return sequelize.models.quiz.bulkCreate([
+        {question: "Capital de Italia", answer:"Roma"},
+        {question: "Capital de Francia", answer:"París"},
+        {question: "Capital de España", answer:"Madrid"},
+        {question: "Capital de Portugal", answer:"Lisboa"}
+    ])
+}
+})
+.catch(error=>{
+    console.log(error);
+});
+
+
 // Create tables
 sequelize.sync()
-.then(() => console.log('Data Bases created successfully'))
+    .then(() => console.log('Data Bases created successfully'))
 .catch(error => {
     console.log("Error creating the data base tables:", error);
-    process.exit(1);
+process.exit(1);
 });
 
 
